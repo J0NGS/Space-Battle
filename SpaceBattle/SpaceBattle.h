8644457fccf4ba84ab4a3f7a0e4d0406db0c07d3
J0NGS <jnetogoncalo@gmail.com>
2022-11-16 14:20:30 -0300

    inicialização

---
+++ SpaceBattle/SpaceBattle.h
@@ -0,0 +1,56 @@
+/**********************************************************************************
+// GeoWars (Arquivo de Cabeçalho)
+//
+// Criação:     15/11/2022
+// Atualização: 15/11/2022
+// Compilador:  Visual C++ 2022
+//
+// Descrição:   Arquivo de cabeçalho do jogo space battle
+//
+**********************************************************************************/
+
+#ifndef _GEOWARS_H_
+#define _GEOWARS_H_
+
+// ------------------------------------------------------------------------------
+
+#include "Game.h"
+#include "Audio.h"
+#include "Scene.h"
+#include "Background.h"
+
+
+// ------------------------------------------------------------------------------
+
+enum ObjectIds { PLAYER, MISSILE };
+
+// ------------------------------------------------------------------------------
+
+enum SoundIds { };
+
+// ------------------------------------------------------------------------------
+
+class SpaceBattle : public Game
+{
+private:
+    Background* bg = nullptr;       // plano de fundo
+    //Hud * hud = nullptr
+    bool viewBBox = false;          // visualização das bbox
+    
+public:
+   
+    //static Player * player;
+    
+    static Audio* audio;            // sistema de audio do jogo
+    static Scene* scene;            // gerenciador de cena do jogo
+    static bool viewHUD;            // visualização do HUD
+
+    void Init();                    // inicialização
+    void Update();                  // atualização
+    void Draw();                    // desenho
+    void Finalize();                // finalização
+};
+
+// ---------------------------------------------------------------------------------
+
+#endif
